71c12a79fb2c5382e73f8bb5036c61062149986a
Andrew D. Horchler <adh9@case.edu>
2013-04-21 15:49:10 -0400

    New functions to calculate period and noise magnitude based on mean period constant.
    Simple functions with no input checking. These use Tau_Bar constant from shc_lv_meanperiod().
    
    Signed-off-by: Andrew D. Horchler <adh9@case.edu>

---
+++ SHCTools/shc_lv_epsilonfit.m
@@ -0,0 +1,17 @@
+function epsilon=shc_lv_epsilonfit(epsilon_hat,tau,tau_bar,lambda_u)
+%SHC_LV_EPSILONFIT  Noise magnitude as a function of fitted constants.
+%   EPSILON = SHC_LV_EPSILONFIT(EPSILON_HAT,TAU,TAU_BAR,LAMBDA_U)
+%
+%   See also:
+%       SHC_LV_TAUFIT, EPSILONFIT, TAUFIT, TP2EPSILON, EPSILON2TP, TP2DELTA,
+%       TP2LAMBDA, STONEHOLMESINVPASSAGETIME, STONEHOLMESPASSAGETIME
+
+%   Andrew D. Horchler, adh9 @ case . edu, Created 4-21-13
+%   Revision: 1.0, 4-21-13
+
+
+% Epsilon(i+1) = F(Tau(i), Tau_Bar(i), Lambda_U(i))
+epsilon = epsilonfit(epsilon_hat([2:end 1]),tau,tau_bar,lambda_u);
+
+% Epsilon(i+1) = F(Tau(i), Tau_Bar(i), Lambda_U(i))
+epsilon = epsilon([end 1:end-1]);
---
+++ SHCTools/shc_lv_taufit.m
@@ -0,0 +1,14 @@
+function tau=shc_lv_taufit(tau_bar,epsilon,epsilon_hat,lambda_u)
+%SHC_LV_TAUFIT  Mean period as a function of fitted constants.
+%   TAU = SHC_LV_TAUFIT(NET,TAU_BAR,EPSILON,EPSILON_HAT,LAMBDA_U)
+%
+%   See also:
+%       SHC_LV_EPSILONFIT, TAUFIT, EPSILONFIT, EPSILON2TP, TP2EPSILON, TP2DELTA,
+%       TP2LAMBDA, STONEHOLMESINVPASSAGETIME, STONEHOLMESPASSAGETIME
+
+%   Andrew D. Horchler, adh9 @ case . edu, Created 4-21-13
+%   Revision: 1.0, 4-21-13
+
+
+% Tau(i) = F(Epsilon(i+1), Epsilon_Hat(i+1))
+tau = taufit(tau_bar,epsilon([2:end 1]),epsilon_hat([2:end 1]),lambda_u);
