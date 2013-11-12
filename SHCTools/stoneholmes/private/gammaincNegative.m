77c98b10935ba5afdbe16479036ac153d7eb5c8d
Andrew D. Horchler <adh9@case.edu>
2013-11-11 19:57:09 -0500

    Greatly improved incomplete gamma function for negative values.
    gammaincNegative now uses MuPAD's igamma exclusively for negative a values (or complex inputs). Performance is improved via the map function which and be applied to either input. Maybe slower than old one, but much more accurate, bug free, and faster than a naive use of igamma.
    
    Signed-off-by: Andrew D. Horchler <adh9@case.edu>

--- SHCTools/stoneholmes/private/gammaincNegative.m
+++ SHCTools/stoneholmes/private/gammaincNegative.m
@@ -1,9 +1,10 @@
-function y=gammaincNegative(x,a,tail)
+function y=gammaincNegative(x,a,varargin)
 %GAMMAINCNEGATIVE  Evaluates the unscaled incomplete gamma function for all A.
 %   Y = GAMMAINCNEGATIVE(X,A) returns the lower incomplete gamma function not
-%   scaled by 1/GAMMA(A). X and A must be real and the same size (or either can
-%   be a scalar). NaN is returned for X < 0 or X = 0 and A not 0 or a negative
-%   integer. The unscaled lower incomplete gamma function is defined as:
+%   scaled by 1/GAMMA(A). X and A must be the same size (or either can be a
+%   scalar) and non-sparse. If X and A are real and the elements of A are
+%   nonnegative, GAMMAINC is used. The unscaled lower incomplete gamma function
+%   is defined as:
 %
 %       gammaincNegative(X,A) = integral from 0 to x of t^(a-1) exp(-t) dt
 %
@@ -21,101 +22,63 @@ function y=gammaincNegative(x,a,tail)
 %   Based on: http://functions.wolfram.com/GammaBetaErf/Gamma2/16/01/01/
 
 %   Andrew D. Horchler, adh9 @ case . edu, Created 8-5-12
-%   Revision: 1.0, 4-7-13
+%   Revision: 1.1, 11-11-13
 
 
-if isscalar(a)
-    y = NaN(size(x),superiorfloat(a,x));
+if ~isfloat(x) || ~isfloat(a) || issparse(x) || issparse(a)
+    error('gammaincNegative:InvalidDatatype',...
+          'Inputs must be floating-point and non-sparse.');
+end
+
+isAReal = (isreal(a) || all(imag(a(:)) == 0));
+if all(a(:) >= 0) && isAReal && (isreal(x) || all(imag(x(:)) == 0))
+    y = gammainc(real(x),real(a),varargin{:}).*gamma(real(a));
 else
-    y = NaN(size(a),superiorfloat(a,x));
-end
-
-i = find(x > 0 | (x == 0 & (a > 0 | a ~= floor(a))));
-if ~isempty(i)
-    if isscalar(a) && isscalar(x)
-        y = gammainc_recurse(a,x);
-    elseif isscalar(a) && ~isscalar(x)
-        y(i) = gammainc_recurse(a,x(i));
-    elseif isscalar(x)
-        for j = 1:numel(a(i))
-            y(i(j)) = gammainc_recurse(a(i(j)),x);
+    if isempty(a)
+        if isscalar(x)
+            y = a;
+        else
+            error('gammaincNegative:EmptyInputA','Input sizes must match.');
         end
-    else
-        for j = 1:numel(a(i))
-            y(i(j)) = gammainc_recurse(a(i(j)),x(i(j)));
-        end
-    end
-    
-    if nargin < 3 || strcmpi(tail,'lower')
+        return;
+    elseif isempty(x)
         if isscalar(a)
-            y(i) = gamma(a)-y(i);
+            y = x;
         else
-            y(i) = gamma(a(i))-y(i);
+            error('gammaincNegative:EmptyInputX','Input sizes must match.');
         end
-    end
-end
-
-
-function y=gammainc_recurse(a,x)
-if a == 0
-    y = expint(x);
-elseif a > 0
-    i = (x == 0);
-    if any(i)
-        y(i) = gamma(a);
+        return;
     end
     
-    i = ~i;
-    if any(i)
-        y(i) = gamma(a).*gammainc(x(i),a,'upper');
-    end
-elseif a < 0
-    i = (x == 0 | x == Inf);
-    if any(i)
-        if a < -171 || x == Inf
-            y(i) = 0;
-        else
-            y(i) = -pi*csc(-pi*a)/gamma(1-a);
+    dtype = superiorfloat(x,a);
+    xs = char(vpa(sym(x)));
+    if isscalar(x)
+        y = cast(evalin(symengine,...
+            ['eval(map(' char(vpa(sym(a))) ',s->igamma(s,' xs ')))']),dtype);
+    elseif isscalar(a)
+        y = cast(evalin(symengine,...
+            ['eval(map(' xs ',t->igamma(' char(vpa(sym(a))) ',t)))']),dtype);
+    else
+        if ndims(x) ~= ndims(a) || ~all(size(x) == size(a))
+            error('gammaincNegative:DimensionMismatch',...
+                  'Inputs must have the same dimensions.');
+        end
+        
+        ys = evalin(symengine,['eval(map(' xs ',t->igamma(s,t)))']);
+        y = zeros(size(a),dtype);
+        for i = 1:numel(a)
+            y(i) = subs(ys(i),'s',a(i));
         end
     end
     
-    i = (~i & a < -5 & x > 10 & x > -0.5*a);
-    if any(i)
-        % Asymtotic series expansion from Gautschi (1959)
-        xi = x(i);
-        z = (xi-a).^2;
-        y(i) = exp(-xi).*xi.^a.*(1-xi./z+xi.*(2*xi+a)./z.^2-xi.*(6*xi.^2 ...
-            +8*a*xi+a^2)./z.^3+xi.*(24*xi.^3+58*a*xi.^2+22*a^2*xi...
-            -a^3)./z.^4)./(xi-a);
-    end
-    
-    i = (~i & x > 0);
-    if any(i)
-        xi = x(i);
-        n = -floor(a);
-        an = 1-a-n;
-        
-        k = n-1:-1:0;
-        if n > 150
-            ga = gammaln(an);
-            ank = exp(gammaln(an+k)-ga);
-            p = (-1)^-n*exp(ga-gammaln(1-a));
+    if nargin < 3 || strcmp(varargin{1},'lower')
+        if isAReal
+            y = y-gamma(a);
         else
-            ga = gamma(an);
-            ank = gamma(an+k)/ga;
-            p = (-1)^-n*ga/gamma(1-a);
+            y = y-cast(gamma(vpa(sym(a))),dtype);
         end
-        
-        for j = numel(xi):-1:1
-            v = (-xi(j)).^-k.*ank;
-            s(j) = sum(v(isfinite(v)));
-        end
-        
-        ex = exp(-xi).*xi.^-an.*s;
-        if an == 1
-            y(i) = (expint(xi)-ex)*p;
-        else
-            y(i) = (gammainc_recurse(a+n,xi)-ex)*p;
-        end
+    elseif ~strcmp(varargin{1},'upper')
+        error('gammaincNegative:InvalidTail',...
+              'Tail must be ''upper'' or ''lower'' (default).');
     end
 end
