5b61f1037a2ef5ad9e5ea68ac269fea4ccffdc4c
Andrew D. Horchler <adh9@case.edu>
2014-06-15 22:13:44 -0400

    Minor fix for robustness
    Ensured that only first component of epsilon is used if a vector is passed in.
    
    Signed-off-by: Andrew D. Horchler <adh9@case.edu>

--- SHCTools-Horchler_etal_2014/shc_lv_ic.m
+++ SHCTools-Horchler_etal_2014/shc_lv_ic.m
@@ -2,9 +2,10 @@ function a0=shc_lv_ic(dt,rho,alpha,epsilon)
 %SHC_LV_IC  Find initial condition on Lotka-Volterra SHC manifold.
 %   A0 = SHC_LV_IC(DT,RHO,ALPHA,EPSILON) returns an initial condition A0 on the
 %   manifold of the Lotka-Volterra SHC defined by the N-dimensional connection
-%   matrix RHO, growth rates ALPHA, and noise magnitudes EPSILON. ALPHA and
-%   EPSILON must be finite real floating-point scalars or length N vectors. DT
-%   is a positive floating-point scalar specifying the integration step size.
+%   matrix RHO, growth rates ALPHA, and noise magnitude EPSILON. ALPHA must be a
+%   finite real floating-point scalar or length N vector. EPSILON must be a
+%   finite real floating-point scalar. DT is a positive floating-point scalar
+%   specifying the integration step size.
 %
 %   The initial condition found will lie along the incomming edge of the
 %   neighborhood of the first node, i.e., A0(1) = BETA(1)-DELTA(1), where BETA
@@ -20,7 +21,7 @@ function a0=shc_lv_ic(dt,rho,alpha,epsilon)
 %   Differential Equations," Springer-Verlag, 1992.
 
 %   Andrew D. Horchler, adh9 @ case . edu, Created 5-29-14
-%   Revision: 1.0, 6-14-14
+%   Revision: 1.0, 6-15-14
 
 
 n = size(rho,1);
@@ -37,15 +38,15 @@ delta = shc_lv_neighborhood(beta);
 % Find undefined initial condition using nullcline
 a0n = beta(n)-delta(n);
 a01 = (1+1./nu(1))*delta(1)/2;
-a = [a01;zeros(n-2,1)+epsilon;a0n];
+a = [a01;zeros(n-2,1)+epsilon(1);a0n];
 
 % Final condition at incomming edge of neighborhood for node 1
 af1 = beta(1)-delta(1);
 
-esdt = sqrt(dt).*epsilon(:);
+esdt = sqrt(dt).*epsilon(1);
 while true
     ap = a;
-    a = max(a+a.*(alpha-rho*a)*dt+esdt*randn(n,1),0);
+    a = max(a+a.*(alpha-rho*a)*dt+esdt.*randn(n,1),0);
     if a(1) >= af1
         % Linearly interpolate for a0 at a0(1) = af1
         a0 = ap+(a-ap)*(af1-ap(1))/(a(1)-ap(1));
